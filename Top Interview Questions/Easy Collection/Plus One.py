d518030f95def18398b5f15c9255200202f472c4
leoleo30539 <30406472+leoleo30539@users.noreply.github.com>
2021-02-25 14:08:33 +0800

    Create Plus One.py

---
+++ Top Interview Questions/Easy Collection/Plus One.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
+        if digits[0] == 0:
+            digits = [1] + digits
+        return digits
